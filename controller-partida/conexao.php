81f268ff9eb030b2e31307584a81c0459e2d757d
Yago Augusto <yago.augusto@dinamo.srv.br>
2025-06-13 02:41:12 -0300

    Dev

--- controller-partida/conexao.php
+++ controller-partida/conexao.php
@@ -1,8 +1,11 @@
 
 
 <?php
-$dsn = 'mysql:host=localhost;dbname=rating_beachtennis;charset=utf8';
-$user = 'root';
-$pass = '';
+//$dsn = 'mysql:host=localhost;dbname=rating_beachtennis;charset=utf8';
+$dsn = 'mysql:host=localhost;dbname=u580429014_app;charset=utf8';
+//$user = 'root';
+//$pass = '';
+$user = 'u580429014_app';
+$pass = 'Caninde.123';
 $conn = new PDO($dsn, $user, $pass);
 ?>
