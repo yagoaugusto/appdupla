080ab98021f9523337c05073488329f1ae9d371f
Yago Augusto <yago.augusto@dinamo.srv.br>
2025-06-23 15:11:16 -0300

    dev

--- system-classes/config.php
+++ system-classes/config.php
@@ -7,7 +7,11 @@ define('DB_HOSTNAME', 'localhost');
 //define('DB_DATABASE', 'rating_beachtennis');
 //define('DB_USERNAME', 'root');
 //define('DB_PASSWORD', '');
+
+// Mercado Pago Credentials
+define('MP_ACCESS_TOKEN', 'TEST-6139135356728189-062109-ef809ab857b3c8d286c42e7b39861667-141946278'); // Substitua pelo seu Access Token real
+define('MP_NOTIFICATION_URL', 'http://localhost/APP%20DUPLA/controller-pagamento/webhook.php'); // URL do seu webhook (ajuste se necessário)
+
 define('DB_DATABASE', 'u580429014_app');
 define('DB_USERNAME', 'u580429014_app');
 define('DB_PASSWORD', 'Caninde.123');
-
